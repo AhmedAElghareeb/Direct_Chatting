84836622ef753b48093c9ce7c95f74fa83d3dc53
AhmedElghareebGeex <ahmed.elghareeb@geexar.dev>
2025-04-30 14:19:19 +0300

    Publish new version on huawei store

--- lib/cmd.dart
+++ lib/cmd.dart
@@ -8,3 +8,13 @@
 // => keytool -genkey -v -keystore $env:USERPROFILE\upload-keystore.jks
 //         -storetype JKS -keyalg RSA -keysize 2048 -validity 10000
 //         -alias upload
+
+
+// for google store:
+//flutter.versionName=1.0.2
+// flutter.versionCode=4
+
+
+// for huawei store:
+//flutter.versionName=1.0.0
+// flutter.versionCode=1
